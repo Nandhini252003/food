cf3432ba2815a1f1573b34d9c7afc929cb9866a3
Nandhini252003 <shiny252003@gmail.com>
2024-11-09 15:57:30 +0530

    added category save

--- src/main/java/com/project/foodApp/dao/daoImp/CategoryDaoImp.java
+++ src/main/java/com/project/foodApp/dao/daoImp/CategoryDaoImp.java
@@ -1,4 +1,20 @@
 package com.project.foodApp.dao.daoImp;
 
-public class CategoryDaoImp {
+import com.project.foodApp.dao.CategoryDao;
+import com.project.foodApp.models.entities.Category;
+import com.project.foodApp.repositories.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CategoryDaoImp implements CategoryDao {
+
+    @Autowired
+    private CategoryRepo categoryRepo;
+
+
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepo.save(category);
+    }
 }
--- src/main/java/com/project/foodApp/services/CategoryService.java
+++ src/main/java/com/project/foodApp/services/CategoryService.java
@@ -1,4 +1,13 @@
 package com.project.foodApp.services;
 
+import com.project.foodApp.models.dto.CategoryDto;
+import com.project.foodApp.models.dto.ResponseStructure;
+import com.project.foodApp.models.entities.Category;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    public ResponseEntity<ResponseStructure<Category>> saveCategory(CategoryDto categoryDto, List<Integer> recipeId);
 }
